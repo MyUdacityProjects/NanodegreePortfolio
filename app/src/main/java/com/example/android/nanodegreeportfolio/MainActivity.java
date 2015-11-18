c2894832ec62103c5f1266294fb37fc7675b89c4
Harshita Kasera <harshita.k@flipkart.com>
2015-11-19 01:20:20 +0530

    Created portfolio project

---
+++ app/src/main/java/com/example/android/nanodegreeportfolio/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.nanodegreeportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public String getToastText(String appName){
+        String genericText =  getResources().getString(R.string.genericText);
+        return genericText + " " + appName.toLowerCase() + " app";
+    }
+
+
+
+    public void clickProject(View v) {
+        // 1) Possibly check for instance of first
+        Button b = (Button)v;
+        String appname = b.getText().toString();
+        String toastText = getToastText(appname);
+        Toast.makeText(getApplicationContext(),toastText,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
